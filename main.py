05e877bdab036f98fba1f3f158bc8f896be5ba55
EmersonUrielVargas <emrvargaitaz@gmail.com>
2023-05-22 13:06:38 -0500

    modify body

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ app = FastAPI()
 
 # from app.models import RequestChat
 class RequestChat(BaseModel):
-    id: int
     message: str
 
 
 @app.get('/')
 def startChat():
-    print('request entrada: ')
     return {'message': "Hello World desde chatbot_api"}
 @app.post('/')
-def MakeChat(request: RequestChat, response_model=None):
+def MakeChat(request: RequestChat):
     question = request.message 
     return (formatResponse(question))
