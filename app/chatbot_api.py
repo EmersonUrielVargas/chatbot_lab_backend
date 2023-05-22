ea695eea2a9f6bffb1849849415310fe6eab0a11
Frankma0117 <franmacagu@gmail.com>
2023-05-21 23:48:07 -0500

    Agregar nuevo servicio de chatBot con Corpus

--- app/chatbot_api.py
+++ app/chatbot_api.py
@@ -1,8 +1,13 @@
 from services.chatbotService import formatResponse
+from services.chatBotCorpus import chat
 
 while True:
+    # response = chat()
+    # if response is None:
+    print("Entra en el modelo GPT")
     pregunta = input('Escribe tu pregunta \n') + "Responde en español"
     print(formatResponse(pregunta))
+
 
 
 
